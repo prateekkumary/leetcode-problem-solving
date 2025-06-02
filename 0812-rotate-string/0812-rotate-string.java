b97637a4678d94f9e01879c37707d0af76e4205e
Prateek Kumar Yadav <108705663+prateekkumary@users.noreply.github.com>
2025-06-03 01:27:37 +0530

    Time: 0 ms (100%), Space: 41.6 MB (52.29%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        int l=s.length()-1;
-        for(int i=0;i<=l;i++){
-            String a=s.substring(l-i)+s.substring(0,l-i);
-            if(a.equals(goal)){
-                return true;
-            }
+        if(s.length()!=goal.length()){
+            return false;
         }
-        return false;
+        return (s+s).contains(goal);
+        // int l=s.length()-1;
+        // for(int i=0;i<=l;i++){
+        //     String a=s.substring(l-i)+s.substring(0,l-i);
+        //     if(a.equals(goal)){
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 }
