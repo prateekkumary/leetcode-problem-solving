81dee83f41bbe3e6ba2dae426426fee8ac0a932f
Prateek Kumar Yadav <108705663+prateekkumary@users.noreply.github.com>
2025-07-26 20:41:59 +0530

    Time: 5 ms (24.22%), Space: 46.4 MB (12.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,92 +1,31 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-       int m=nums1.length;
-        int n=nums2.length;
-        int s=m+n;
-        int[] merged=new int[s];
+    public double findMedianSortedArrays(int[] num1, int[] num2) {
+
+        int n=num1.length;
+        int m=num2.length;
+
+        int[] merged=new int[n+m];
         int k=0;
-        for(int i=0;i<m;i++){
-            merged[k++]=nums1[i];
-        }
-        
+
         for(int i=0;i<n;i++){
-            merged[k++]=nums2[i];
+            merged[k++]=num1[i];
         }
-        Arrays.sort(merged);
-        int total=merged.length;
+        for(int i=0;i<m;i++){
+            merged[k++]=num2[i];
+        }
         
+        Arrays.sort(merged);
+
+        int total=merged.length;
+
         if(total%2==1){
             return (double)merged[total/2];
         }
         else{
-            int m1=merged[total/2-1];
-            int m2=merged[total/2];
-            return ((double)m1+(double)m2)/2.0;
+            int mid1=merged[total/2-1];
+            int mid2=merged[total/2];
+            return (mid1+mid2)/2.0;
         }
+        
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import java.util.Arrays;
-
-// class Solution {
-//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-//         // Get the sizes of both input arrays.
-//         int n = nums1.length;
-//         int m = nums2.length;
-
-//         // Merge the arrays into a single sorted array.
-//         int[] merged = new int[n + m];
-//         int k = 0;
-//         for (int i = 0; i < n; i++) {
-//             merged[k++] = nums1[i];
-//         }
-//         for (int i = 0; i < m; i++) {
-//             merged[k++] = nums2[i];
-//         }
-
-//         // Sort the merged array.
-//         Arrays.sort(merged);
-
-//         // Calculate the total number of elements in the merged array.
-//         int total = merged.length;
-
-//         if (total % 2 == 1) {
-//             // If the total number of elements is odd, return the middle element as the median.
-//             return (double) merged[total / 2];
-//         } else {
-//             // If the total number of elements is even, calculate the average of the two middle elements as the median.
-//             int middle1 = merged[total / 2 - 1];
-//             int middle2 = merged[total / 2];
-//             return ((double) middle1 + (double) middle2) / 2.0;
-//         }
-//     }
-// }
